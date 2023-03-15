0ff7540b5cd5d981790c5e89dc5126fb51ada4a4
Eric_Lian <git@superexercisebook.com>
2023-03-15 12:02:29 +0800

    feat: 添加一些东西

--- util/model.go
+++ util/model.go
@@ -3,6 +3,7 @@ package util
 // MyAwesomeModel 是一个很酷炫的结构体
 // Id 和 Name 都是我胡诌的，反正能跑就是胜利
 type MyAwesomeModel struct {
-	Id   string // Id 是一个很酷炫的 Id
-	Name string // Name 是一个很酷炫的 Name
+	Id        string      // Id 是一个很酷炫的 Id
+	Name      string      // Name 是一个很酷炫的 Name
+	ExtraInfo interface{} // ExtraInfo 我看不懂但是我大受震撼
 }
