1f1bd752bdbadf2246998e47f793977fe3486138
Eric_Lian <git@superexercisebook.com>
2023-03-17 14:30:25 +0800

    docs: 试试文档换行

--- util/model.go
+++ util/model.go
@@ -1,6 +1,7 @@
 package util
 
 // MyAwesomeModel 是一个很酷炫的结构体
+//
 // Id 和 Name 都是我胡诌的，反正能跑就是胜利
 type MyAwesomeModel struct {
 	Id        string      // Id 是一个很酷炫的 Id
--- util/os.go
+++ util/os.go
@@ -4,7 +4,9 @@ package util
 import "os"
 
 // GetEnvVar 获取环境变量
+//
 // 当 varName 表示的环境变量存在且不为空字符串时返回环境变量 varName 的值
+//
 // 当 varName 表示的环境变量不存在或为空时返回 defaultVal 的值
 func GetEnvVar(varName string, defaultVal string) string {
 	ret, exist := os.LookupEnv(varName)
@@ -15,6 +17,7 @@ func GetEnvVar(varName string, defaultVal string) string {
 }
 
 // GetEnvVarAlternative 获取环境变量
+//
 // 获取优先级为 varName[0] 到 varName[len(varName) - 1]，最后添加 defaultVal
 func GetEnvVarAlternative(varName []string, defaultVal func() string) string {
 	for _, it := range varName {
